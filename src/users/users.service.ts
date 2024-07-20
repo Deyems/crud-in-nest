701672dc06b68459be8fe5976aa01ba3fb0766f4
Deyems <adeyemisola11@gmail.com>
2024-07-20 18:25:54 +0100

    delete, update, create methods and enum for findAll added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -35,7 +35,7 @@ export class UsersService {
     },
   ];
 
-  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN'){
+  findAll(role?: 'INTEGER' | 'ENGINEER' | 'ADMIN'){
     if (role) {
       return this.users.filter((user) => user.role === role);
     }
@@ -45,4 +45,30 @@ export class UsersService {
   findOne(id: number){
     return this.users.find((user) => user.id == id);
   }
+
+  create(user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
+    const userByHighestId = [...this.users].sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: userByHighestId[0].id + 1,
+      ...user,
+    }
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, updatedUser: {name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
+    this.users = this.users.map(user => {
+      if (user.id === id) {
+        return {...user, ...updatedUser};
+      }
+      return this.findOne(id);
+    });
+  }
+
+  delete(id: number){
+    const removedUser = this.findOne(id);
+    this.users.filter(user => user.id !== id);
+    return removedUser;
+  }
+
 }
